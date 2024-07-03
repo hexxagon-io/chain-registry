fdec258e4237e53b027c9e9ab67862085a8b168b
echel0n-HX <echel0n@hexxagon.io>
2024-07-02 22:52:48 -0700

    updated gas prices for comdex

--- chains/mainnet/comdex.js
+++ chains/mainnet/comdex.js
@@ -2,7 +2,7 @@ module.exports = {
     chainID: 'comdex-1',
     lcd: 'https://comdex-api.polkachu.com',
     gasAdjustment: 1.75,
-    gasPrices: {ucmdx: 0.025},
+    gasPrices: {ucmdx: 0.03},
     prefix: 'comdex',
     coinType: '118',
     baseAsset: 'ucmdx',
