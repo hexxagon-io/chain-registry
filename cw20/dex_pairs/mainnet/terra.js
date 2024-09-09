521141cd7c744255ac0c628e816bb431372c8a85
Gerry <weegerryogle@protonmail.com>
2024-09-09 12:26:42 +0100

    adding records for Alpha

--- cw20/dex_pairs/mainnet/terra.js
+++ cw20/dex_pairs/mainnet/terra.js
@@ -1160,4 +1160,13 @@ module.exports = [
             "uluna"
         ]
     },
+    {
+        token: "terra12u7khzrzn05a73xkpq6a5zrcazz2xmqn7lvupmqmca06pgcyt5qsa9e7p6",
+        dex: "terraport",
+        type: "xyk",
+        assets: [
+            "terra1x6e64es6yhauhvs3prvpdg2gkqdtfru840wgnhs935x8axr7zxkqzysuxz",
+            "uluna"
+        ]
+    },
 ]
