099f388b53878faecbe779ca17cef773bd5d5694
echel0n-HX <138946701+echel0n-HX@users.noreply.github.com>
2024-10-16 22:51:33 -0700

    Update terra.js

--- cw20/contracts/mainnet/terra.js
+++ cw20/contracts/mainnet/terra.js
@@ -2225,12 +2225,6 @@ module.exports = [
         icon: "https://i.ibb.co/hWH7rzM/photo-2024-04-06-09-51-19-1.png"
     },
     {
-        contract: "terra1vhgq25vwuhdhn9xjll0rhl2s67jzw78a4g2t78y5kz89q9lsdskq2pxcj2",
-        protocol: "Juris",
-        name: "Juris Protocol",
-        icon: "https://24.143.179.94/juris-internal/resource/img/juris-logo.png"
-    },
-    {
         contract: "terra1m58hc296srr6xygrrfyu6u0e32pl8d459nfs55pmegke8zlv94rqsjz882",
         protocol: "CatWifHat",
         name: "CatWifHat",
