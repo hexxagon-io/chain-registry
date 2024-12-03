78697d1fd58c22a05455eda4de5a38ec9c2a0431
Garuda <139744237+garudawork@users.noreply.github.com>
2024-12-03 11:05:33 +0800

    Update terra.js

--- cw20/tokens/mainnet/terra.js
+++ cw20/tokens/mainnet/terra.js
@@ -2020,4 +2020,12 @@ module.exports = [
         icon: "https://i.ibb.co.com/8c1fmGM/IMG-20240921-202013-833.jpg",
         decimal: 6,
     },
+    {
+        protocol: "SANTA KLAUS",
+        name: "KLAUS",
+        symbol: "SANTAKLAUS",
+        token: "terra15juucad3k2npsj53crl6qhra9f65s9jkas9nzpqypj38en4ht32qczde4a",
+        icon: "https://i.ibb.co.com/8c1fmGM/IMG-20240921-202013-833.jpg",
+        decimal: 6,
+    },
 ]
