d297c6e5eff50e472ab8ea00ea13afcf24b5879d
echel0n <echel0n@hexxagon.io>
2024-12-19 08:29:52 -0800

    fixed tax rate for classic

--- chains/mainnet/terra.js
+++ chains/mainnet/terra.js
@@ -3,7 +3,7 @@ module.exports = {
     lcd: 'https://lcd.terra-classic.hexxagon.io',
     api: 'https://api.terra-classic.hexxagon.io',
     hive: 'https://hive.terra-classic.hexxagon.io/graphql',
-    taxRate: 0.05,
+    taxRate: 0.005,
     gasAdjustment: 2,
     gasPrices: {
         uluna: 28.325,
