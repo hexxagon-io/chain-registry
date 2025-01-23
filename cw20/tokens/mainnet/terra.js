6cabe1fd549c2db461524880d8bc0be62bf0f052
echel0n <echel0n@hexxagon.io>
2025-01-22 16:07:07 -0800

    added WESO token

--- cw20/tokens/mainnet/terra.js
+++ cw20/tokens/mainnet/terra.js
@@ -2092,5 +2092,13 @@ module.exports = [
         token: "terra1x6e64es6yhauhvs3prvpdg2gkqdtfru840wgnhs935x8axr7zxkqzysuxz",
         icon: "https://i.ibb.co/3ytCxdy/alpha-130-min.png",
         decimals: 6,
+    },
+    {        
+        protocol : "WESO Token",
+        name: "WESO Token",
+        symbol: "WESO",
+        token: "terra13ryrrlcskwa05cd94h54c8rnztff9l82pp0zqnfvlwt77za8wjjsld36ms",
+        icon: "https://i.ibb.co/G2y0wf5/n-Y-sl6-YZ-400x400.jpg",
+        decimals: 6,
     }
 ]
