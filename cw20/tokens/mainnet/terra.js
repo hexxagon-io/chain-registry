3d8e4993a8318c7c011b169081286702b031d9d9
Garuda <139744237+garudawork@users.noreply.github.com>
2025-09-03 13:44:27 +0800

    Update terra.js

--- cw20/tokens/mainnet/terra.js
+++ cw20/tokens/mainnet/terra.js
@@ -2181,4 +2181,12 @@ module.exports = [
         icon: "https://i.ibb.co/DfDtjLgw/amp-NIP-NFT.png",
         decimals: 6,
     },
+     {
+        protocol : "LUNCMOON",
+        name: "LUNCMOON",
+        symbol: "LCM",
+        token: "terra1zra7m9w59sxwxqy8lqs3yeqleedrvtgg2qf938nk2wf8pq2ejkqsn0heln",
+        icon: "https://i.ibb.co/HD9PzY3x/IMG-9052.jpg",
+        decimals: 6,
+    },
 ]
