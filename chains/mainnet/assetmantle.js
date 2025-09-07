f6dfc479a43b1918e64882802659893a31f0cb25
Hexxagon API <api@hexxagon.io>
2025-09-07 07:25:38 +0000

    Added chain: assetmantle

---
+++ chains/mainnet/assetmantle.js
@@ -0,0 +1,39 @@
+module.exports = {
+  "chainID": "mantle-1",
+  "lcd": "https://rest.assetmantle.one",
+  "rpc": "https://rpc.assetmantle.one",
+  "gasAdjustment": 1.75,
+  "gasPrices": {
+    "umntl": 0.025
+  },
+  "prefix": "mantle",
+  "coinType": "118",
+  "baseAsset": "umntl",
+  "name": "AssetMantle",
+  "chainRegistryName": "assetmantle",
+  "disabledModules": [
+    "swap"
+  ],
+  "icon": process.env.CF_PAGES_URL + '/img/chains/assetmantle.svg',
+  "gitRepo": "https://github.com/AssetMantle/node.git",
+  "autoCompound": {
+    "enabled": true,
+    "minRewards": 1000000
+  },
+  "explorer": {
+    "address": "https://explorer.chainroot.io/assetmantle/accounts/{}",
+    "tx": "https://explorer.chainroot.io/assetmantle/transactions/{}",
+    "validator": "",
+    "block": ""
+  },
+  "tokens": [
+    {
+      "token": "umntl",
+      "symbol": "MNTL",
+      "name": "AssetMantle",
+      "icon": process.env.CF_PAGES_URL + '/img/coins/MNTL.svg',
+      "decimals": 6,
+      "coinGeckoID": "assetmantle"
+    }
+  ]
+};
