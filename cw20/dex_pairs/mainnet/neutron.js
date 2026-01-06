70a6a926a863e160b661cea1763c683a3978ea93
echel0n <echel0n@hexxagon.io>
2026-01-05 17:17:13 -0800

    added neutron mainnet astroport dex pairs

---
+++ cw20/dex_pairs/mainnet/neutron.js
@@ -0,0 +1,101 @@
+module.exports = [
+  {
+    token: 'neutron1ug2kdpsvdya0ze0rffzyqru9tjtkrm8yr9zse6f85usyf48caq5sh2z3ay',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron13vg3sjfa4l7qv53jhjstd5vml2xxag8xp3m3pfjvtg7sx0k9v2vqg7a2y3',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1p3tydujy66u2d5t7tprr4045zs2z83e93z66vf7jcc9a70um53xqzn3dk5',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1hz5upuygfdukc3h43fag59mm6g2s60qflejmcud5whd38euupevsd9kp5v',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1nak996aj387z77qdpra3hs3jsuzjpv5nxxjypsjp0z28k66lvxas92ajf6',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1feg65h4ljx3qy4m7zvflp8ujgqvp0fz53cz6d485t54gscrmhwdqxdv3yg',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1jtdcd362hurqxyvqzrx284jz787xyxnn3fazhez0gxxv5l5wh4ysn9kza6',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1aw7r4gr83j2jjzj02j8eg8vmshzqj0a4rsdjhufw9hlryphswn3q7wex02',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1a4uqzd27gwgam99hugllyhqj24la05pev3m7huecnzvgvruagh3qgdcd07',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1ydjtjhhsmh909e9ahu746qdhd0hppx03pawq28xcqec4vez4j24qz9g0jv',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1qg3eh22sy4jeushxpu9zu73ntmn3e8468rwqdpc4s0g5j80lnkfqzfp3qe',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron12ve9fafr8lj4k3nf6vmz82u3ulmmr80ut697ygq8j0ksudvv0csqfkkh2p',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1tp0axdw2efsv2wdvwkxkk4fkrwxy4a8s9dv5x62ja70mcrztau6spw43c0',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1m2yxg7q9zcjdkga6hwu7xg4d35p3q9c8ugrue0qu0pqajdpz3g7q8lavst',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1y9y9wn09v0s7ext2ax5e9akw8yy86gl492lr6mneesp3wdqec5yqea0837',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron1ppajkmsnkhygztka6zgzf3d25df623l6emftypr6gel5tjunmntqnxte6d',
+      'untrn'
+    ]
+  },
+  {
+    token: 'neutron1735zlph2mjtx4s3pphuh2lmktmhx0crf2z5v7xau3n6qchdxrc5qalk4xh',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'factory/neutron1ffus553eet978k024lmssw0czsxwr97mggyv85lpcsdkft8v9ufsz3sa07/astro',
+      'neutron1fe4fd33d0976gqr833xhdxmw5hv8c5s4nv7pvdrdflpr8u4uadjq76tmdq'
+    ]
+  },
+  {
+    token: 'neutron1a7ge6x7yf0dtx543ecjqr9e6f7rdr8xnc8dluk3dkx7kd4pgcumqee48w3',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'factory/neutron10sr06r3qkhn7xzpw3339wuj77hu06mzna6uht0/eclip',
+      'neutron10qptxs0v6r5w8d5hej0w9nucct7ewshnn23cn9evdp9xq26gaagsjlr9gg'
+    ]
+  },
+  {
+    token: 'neutron1dac6zred7p98csvdfxk3k7xtmh9q53nk4mnqkamv460ct7k3e3wsawspdy',
+    dex: 'astroport',
+    type: 'xyk',
+    assets: [
+      'neutron19mnfnesrhgvp3tm6aedvexth63tyffmyslkpg94yfhyamqsdav7q305zhd',
+      'factory/neutron1ffus553eet978k024lmssw0czsxwr97mggyv85lpcsdkft8v9ufsz3sa07/astro'
+    ]
+  }
+];
